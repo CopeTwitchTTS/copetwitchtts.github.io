ea4ea877c46f39f801ffac2e8a681faddbd190f2
CopeTwitchTTS <165660969+CopeTwitchTTS@users.noreply.github.com>
2024-04-04 19:55:55 +0200

    Changed link handling

--- client.js
+++ client.js
@@ -65,15 +65,16 @@ document.getElementById("play").addEventListener("click", () =>
 
     client.on("message", (channel, tags, message, self) =>
     {
-        const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-        if(message.match(new RegExp(regex)))
-            return;
-
         if(excluded.includes(tags["display-name"]))
             return;
 
-        //handleMessage(tags["display-name"], tags["color"], message);
-
+        const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+        if(message.match(new RegExp(regex)))
+        {
+            handleMessage(tags["display-name"], tags["color"], `${tags["display-name"]} sent a link`);
+            return;
+        }
+        
         while(true)
         {
             if(message.length < 200)
